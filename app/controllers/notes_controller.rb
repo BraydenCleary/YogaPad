8c382c546ee48aae3821621c6890c4f93d754df4
Brayden Cleary <brayden.cleary@gmail.com>
2013-03-18 22:35:18 -0700

    Wrote create action for controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,5 +4,15 @@ class NotesController < ApplicationController
     @note  = Note.new
     @notes = Note.all 
   end
+
+  def create
+    note = Note.new(params[:note])
+    if note.save
+      flash[:success] = 'Note successfully created!'
+      redirect_to notes_path
+    else
+      render 'index'
+    end
+  end
   
 end
