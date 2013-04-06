4890c8fbf62729aa8339f417cfbb9f1e0aae0f3f
Brayden Cleary <brayden.cleary@gmail.com>
2013-04-06 16:19:54 -0700

    Only display 5 yelp results and user can choose to render more

--- app/assets/javascripts/yelp.js
+++ app/assets/javascripts/yelp.js
@@ -1,3 +1,33 @@
+var Results = {
+	init: function(results){
+		this.results = this.splitResults(results, 3);
+	},
+	splitResults: function(source, groups) {
+		var grouped = [];
+		groupSize = Math.ceil(source.length/groups);
+   	var queue = source;
+   	for (var r=0;r<groups;r++) {
+    	grouped.push(queue.splice(0, groupSize));
+    }
+    return grouped;
+	},
+	displayMore: function(){
+		if (this.results.length > 0){
+			$('.yelp-results').append("<span class='more'>MORE</span>")
+		}
+	},
+
+	renderResults: function(){
+		var initialResults = this.results.splice(0,1)[0]
+		for (i in initialResults){
+			var result = new Result(initialResults[i]);
+			result.render();
+		}
+		this.displayMore();
+	}
+
+}
+
 function Result(search_result){
 	this.name = 				 search_result.name
 	this.rating = 			 search_result.rating
@@ -24,12 +54,17 @@ $(document).ready(function(){
 			data: $(this).serialize(),
 			success: function(response){
 				var search_results = response.businesses
-				$('.yelp-results').text('');
-				for (var i in search_results){
-					var result = new Result(search_results[i]);
-					result.render();
-				}
+				Results.init(search_results)
+				Results.renderResults();
 			}
 		});
 	});
+
+	$('ul').on('click', function(e){
+		if ($(e.target).attr('class') == 'more'){
+			$(e.target).hide();
+			Results.renderResults();
+		}
+	})
+
 });
