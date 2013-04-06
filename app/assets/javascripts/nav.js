5d3680991e5195479ba6b9460835b99a25a6c92c
Brayden Cleary <brayden.cleary@gmail.com>
2013-04-05 17:57:38 -0700

    Removing users form app

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
 
-	$('#signup-toggle').on('click', function(){
-		$('#signup-form').slideToggle();
-	});
-
-	$('#signup-trigger').on('click', function(){
-		$('#signup-form').slideToggle();
-	});
-
-	$('#signin-toggle').on('click', function(){
-		$('#signin-form').slideToggle();
-	})
-
 	$('#yelp').on('click', function(){
 		$('.yelp-search').slideToggle();
 	})
