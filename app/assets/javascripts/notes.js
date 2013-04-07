37970c8fe8935450183c2fd205ebcafae5e490cd
Brayden Cleary <brayden.cleary@gmail.com>
2013-04-07 15:02:16 -0700

    Created notesView JS object that renders each note on the index page

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,46 +1,70 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+notesView = {
+  setNotes: function(){
+    this.notes = JSON.parse($('[data-notes]').text())
+  },
 
-function noteView(note) {
-	this.id = note.id
-	this.text = note.text
-	this.created_at = new Date(note.created_at).toDateString()
-	this.delete_button = "<a href='/notes/" + this.id + "' " + "class='likeabutton' data-method='delete' rel='nofollow'>Remove</a></li>"
+  init: function(){
+    this.template = $('.notes').children();
+    this.setNotes();
+    this.listenForDelete();
+    $('.notes').html('')
+  },
 
-	this.render = function() {
-		return ("<li>" + this.text + " " + this.created_at + this.delete_button + "</li>")
-	}
+  renderNotes: function(){
+    for (i in this.notes){
+      note = new noteView(this.notes[i], this.template.clone())
+      note.render2();
+      $('.notes').append(note.template)
+    }
+  },
+
+  listenForDelete: function(){
+    $(".notes").on('click', function(event){
+      if ($(event.target).is("a[data-method='delete']")){
+        event.stopPropagation();
+        event.preventDefault();
+        var url = $(event.target).attr('href');
+        $(event.target).closest('li').hide();
+        $.ajax({
+          url: url,
+          type: 'POST',
+          data: { "_method":"delete"},
+          success: function(response){
+            console.log('Note deleted')
+          }
+        });
+      }
+    });
+  }
+}
+
+
+function noteView(note, template) {
+  this.id = note.id;
+  this.text = note.text;
+  this.created_at = new Date(note.created_at).toDateString();
+  this.template = $(template);
+
+  this.setBody = function(){
+    this.template.find('.note-body').text(this.text)
+  }
+
+  this.setHref = function(){
+    this.template.find('a').attr('href', '/notes/' + this.id)
+  }
+
+  this.render2 = function(){
+    this.setBody();
+    this.setHref();
+    return (this.template)
+  }
 }
 
 
 $(document).ready(function(){
-
-	//Delete notes
-	//should be inside of notes new
-	$(".notes").on('click', function(event){
-		if ($(event.target).is("a[data-method='delete']")){
-			event.stopPropagation();
-			event.preventDefault();
-			var url = $(event.target).attr('href');
-			$(event.target).closest('li').hide();
-			$.ajax({
-				url: url,
-				type: 'POST',
-				data: { "_method":"delete"},
-				success: function(response){
-					console.log('Note deleted')
-				}
-			});
-		}
-	});
-
-	//Render notes
-	//should be inside of notes view
-	var notes = JSON.parse($('[data-notes]').text())
-	for (i in notes){
-		note = new noteView(notes[i])
-		$('.notes').append(note.render())
-	}
-
+  notesView.init();
+  notesView.renderNotes();
 });
 
